10daf120bef848661815fed136772f76b06ff494
Jagadish Chaluvaiah <jagadish404@gmail.com>
2021-10-15 00:56:38 +0800

    Update components to use new redux action creators

--- src/js/components/CartPage.js
+++ src/js/components/CartPage.js
@@ -1,64 +1,66 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import Product from './Product';
-import NoResults from './NoResults';
-import { fetchProducts } from '../actions/productActions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import Product from "./Product";
+import NoResults from "./NoResults";
+import { fetchProducts } from "../actions/productActions";
 
 class CartPage extends Component {
+  componentDidMount() {
+    const { fetchAllProducts } = this.props;
+    fetchAllProducts();
+  }
 
-	componentWillMount() {
-		const { fetchAllProducts } = this.props;
-		fetchAllProducts();
-	}
-
-	render() {
-		const { productsList } = this.props;
-		// productsList = productsList.filter(product => product.isAddedToCart);
-		return (
-			<div className="App-page">
-				<div className="App-header">
-					<Link to="/">
-						<button type="button" className="App-button Cart-button">Browse</button>
-					</Link>
-				</div>
-				<div className="App-body">
-					<div className="Products-grid">
-						{
-							(productsList.length > 0)
-								?	productsList.map((product, index) => (
-									<Product key={`${product.brand}_${product.name}_${product.price}`} {...product} productIndex={index} />
-								))
-								: <NoResults content="No products added to cart!!!!!!!" />
-						}
-					</div>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    const { productsList } = this.props;
+    return (
+      <div className="App-page">
+        <div className="App-header">
+          <Link to="/">
+            <button type="button" className="App-button Cart-button">
+              Browse
+            </button>
+          </Link>
+        </div>
+        <div className="App-body">
+          <div className="Products-grid">
+            {productsList.length > 0 ? (
+              productsList.map((product, index) => (
+                <Product key={`${product.brand}_${product.name}_${product.price}`} {...product} productIndex={index} />
+              ))
+            ) : (
+              <NoResults content="No products added to cart!" />
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-	return {
-		productsList: state.productsData.cartItems
-	};
+  return {
+    productsList: state.cart.items,
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-	return {
-		fetchAllProducts: bindActionCreators(fetchProducts, dispatch)
-	};
+  return {
+    fetchAllProducts: bindActionCreators(fetchProducts, dispatch),
+  };
 }
 
 CartPage.propTypes = {
-	fetchAllProducts: PropTypes.func.isRequired,
-	productsList: PropTypes.arrayOf(PropTypes.shape({
-		brand: PropTypes.string.isRequired,
-		name: PropTypes.string.isRequired,
-		price: PropTypes.string.isRequired
-	})).isRequired
+  fetchAllProducts: PropTypes.func.isRequired,
+  productsList: PropTypes.arrayOf(
+    PropTypes.shape({
+      brand: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      price: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartPage);
--- src/js/components/Product.js
+++ src/js/components/Product.js
@@ -1,69 +1,71 @@
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import * as productActions from '../actions/productActions';
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { addItemToCart } from "../reducers/cartReducer";
 
-import '../../css/productsList.css';
+import "../../css/productsList.css";
 
 class Product extends Component {
-	constructor() {
-		super();
-		this.addToCart = this.addToCart.bind(this);
-	}
+  constructor() {
+    super();
+    this.addToCart = this.addToCart.bind(this);
+  }
 
-	addToCart() {
-		const { addProductToCart, productsList, productIndex } = this.props;
-		addProductToCart(productsList[productIndex]);
-	}
+  addToCart() {
+    const { addItemToCart, productsList, productIndex } = this.props;
+    addItemToCart(productsList[productIndex]);
+  }
 
-	render() {
-		const {
-			productIndex, image, name, price, count
-		} = this.props;
+  render() {
+    const { productIndex, image, name, price, count } = this.props;
 
-		return (
-			<div className="Product-card">
-				<Link to={`/ProductDetails/${productIndex}`}>
-					<img className="Product-icon" alt={image} src={`./assets/${image}`} />
-				</Link>
-				<span className="Product-name">{name}</span>
-				<span className="Product-price">${price}</span>
-				<button type="button" className="App-button Add-to-cart" onClick={this.addToCart}>Add To Cart {count > 0 ? `(${count})` : ''}</button>
-			</div>
-		);
-	}
+    return (
+      <div className="Product-card">
+        <Link to={`/ProductDetails/${productIndex}`}>
+          <img className="Product-icon" alt={image} src={`./assets/${image}`} />
+        </Link>
+        <span className="Product-name">{name}</span>
+        <span className="Product-price">${price}</span>
+        <button type="button" className="App-button Add-to-cart" onClick={this.addToCart}>
+          Add To Cart {count > 0 ? `(${count})` : ""}
+        </button>
+      </div>
+    );
+  }
 }
 
 Product.propTypes = {
-	name: PropTypes.string.isRequired,
-	image: PropTypes.string.isRequired,
-	price: PropTypes.string.isRequired,
-	count: PropTypes.number,
-	productIndex: PropTypes.number.isRequired,
-	addProductToCart: PropTypes.func.isRequired,
-	productsList: PropTypes.arrayOf(PropTypes.shape({
-		brand: PropTypes.string.isRequired,
-		name: PropTypes.string.isRequired,
-		price: PropTypes.string.isRequired
-	})).isRequired
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+  count: PropTypes.number,
+  productIndex: PropTypes.number.isRequired,
+  addItemToCart: PropTypes.func.isRequired,
+  productsList: PropTypes.arrayOf(
+    PropTypes.shape({
+      brand: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      price: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
 
 Product.defaultProps = {
-	count: 0
+  count: 0,
 };
 
 function mapStateToProps(state) {
-	return {
-		productsList: state.productsData.productsList
-	};
+  return {
+    productsList: state.products.productsList,
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-	return {
-		addProductToCart: bindActionCreators(productActions.addProductToCart, dispatch)
-	};
+  return {
+    addItemToCart: bindActionCreators(addItemToCart, dispatch),
+  };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Product);
