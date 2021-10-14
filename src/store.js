6184186f35da33cdc25dfc6bdac048b6455bed56
Jagadish Chaluvaiah <jagadish404@gmail.com>
2021-10-15 00:54:37 +0800

    Update store.js to follow reduxjs/toolkit pattern

--- src/store.js
+++ src/store.js
@@ -1,6 +1,15 @@
-import { applyMiddleware, createStore } from 'redux';
-import createLogger from 'redux-logger';
-import thunk from 'redux-thunk';
-import reducer from './js/reducers';
+import { configureStore } from "@reduxjs/toolkit";
+import { logger } from "redux-logger";
+import thunk from "redux-thunk";
+import productsReducer from "./js/reducers/productReducer";
+import cartReducer from "./js/reducers/cartReducer";
+import filterReducer from "./js/reducers/filterReducer";
 
-export default createStore(reducer, applyMiddleware(thunk, createLogger));
+export default configureStore({
+  reducer: {
+    products: productsReducer,
+    cart: cartReducer,
+    filters: filterReducer,
+  },
+  middleware: [logger, thunk],
+});
