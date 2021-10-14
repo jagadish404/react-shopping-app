1da6b32cc20bedb46d575c032f9ebc033eef613c
Jagadish Chaluvaiah <jagadish404@gmail.com>
2021-10-15 00:51:50 +0800

    Refactor productReducer to follow reduxjs/toolkit format

--- src/js/reducers/productReducer.js
+++ src/js/reducers/productReducer.js
@@ -1,42 +1,54 @@
-import {
-	PRODUCT_LIST_RESPONSE, PRODUCT_ADDED_TO_CART, UPDATE_PRODUCT_FILTER
-} from '../actions/productActions';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-	productsList: [],
-	filters: [],
-	filtersSelected: [],
-	cartItems: []
+  productsList: [],
 };
 
+export const fetchData = createAsyncThunk("products/fetchData", async () => {
+  const data = await fetch("./data/products.json", {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  });
+  const res = await data.json();
+  return res;
+});
 
-export default function reducer(state = initialState, action) {
-	switch (action.type) {
-	case PRODUCT_LIST_RESPONSE: {
-		return {
-			...state,
-			productsList: action.payload.products,
-			filters: action.payload.filters,
-			filtersSelected: action.payload.filtersSelected,
-			cartItems: action.payload.cartItems
-		};
-	}
-	case PRODUCT_ADDED_TO_CART: {
-		return {
-			...state,
-			cartItems: action.payload
-		};
-	}
-	case UPDATE_PRODUCT_FILTER: {
-		return {
-			...state,
-			filtersSelected: action.payload
-		};
-	}
-	default: {
-		return {
-			...state
-		};
-	}
-	}
-}
+const filterProductsByPrice = (value, list) => {
+  let [priceRangeStart, priceRangeEnd] = value.split("-");
+  let filteredProducts = list.filter(({ price }) => {
+    return price >= priceRangeStart && price <= priceRangeEnd;
+  });
+
+  return filteredProducts;
+};
+
+const productSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {
+    filterProductsBy: (state, action) => {
+      let { type, value } = action.payload;
+      let filteredProducts = [];
+
+      if (type === "price") {
+        filteredProducts = filterProductsByPrice(value, state.productsList);
+      } else if (type === "brand") {
+        filteredProducts = state.productsList.filter(({ brand }) => brand === value);
+      } else {
+        filteredProducts = state.productsList;
+      }
+
+      state.productsList = filteredProducts;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchData.fulfilled, (state, action) => {
+      state.productsList = action.payload.products;
+    });
+  },
+});
+
+export const { filterProductsBy } = productSlice.actions;
+export default productSlice.reducer;
