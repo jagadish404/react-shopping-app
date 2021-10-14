deedb60ab9091587cc8954905b84202c85df1a4a
Jagadish Chaluvaiah <jagadish404@gmail.com>
2021-10-15 00:45:56 +0800

    Remove service worker from project

--- src/index.js
+++ src/index.js
@@ -4,12 +4,9 @@ import { Provider } from 'react-redux';
 import store from './store';
 import './css/index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
 	<Provider store={store}>
 		<App />
 	</Provider>, document.getElementById('root')
 );
-
-registerServiceWorker();
