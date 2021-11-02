164a2b5a08f195cd842e313955f71f5de823b888
Jagadish Chaluvaiah <jagadish404@gmail.com>
2021-11-02 17:42:08 +0800

    Update test case for Product component

--- src/js/components/Product.test.jsx
+++ src/js/components/Product.test.jsx
@@ -3,12 +3,21 @@ import { render, screen, fireEvent } from "../utils/test-utils";
 
 import ProductsList from "./ProductsList";
 import productData from "../../../public/data/products.json";
+import Product from "./Product";
 
 beforeEach(() => {
   fetch.resetMocks();
 });
 
 describe("Test suite for Products component", () => {
+  it("Should render component with data provided by props", async () => {
+    fetch.mockResponseOnce(JSON.stringify(productData));
+    render(<Product product={productData.products[0]} productIndex={0} />);
+
+    expect(screen.getByText(productData.products[0].name)).toBeInTheDocument();
+    expect(screen.getByText(`$${productData.products[0].price}`)).toBeInTheDocument();
+  });
+
   it("Should add item to cart and increase the count of items by 1", async () => {
     fetch.mockResponseOnce(JSON.stringify(productData));
     render(<ProductsList />);
