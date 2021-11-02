b0d71c5f9736c014e7b6cf8cb0f9e269d9e52579
Jagadish Chaluvaiah <jagadish404@gmail.com>
2021-11-02 12:36:22 +0800

    Move the test case to Product

--- src/js/components/ProductsList.test.jsx
+++ src/js/components/ProductsList.test.jsx
@@ -29,17 +29,4 @@ describe("Test suite for ProductsList component", () => {
     expect(nutriwellProducts[0]).toBeInTheDocument();
     expect(nutriwellProducts.length).toEqual(2);
   });
-
-  it("Should add item to cart and increase the count of items by 1", async () => {
-    fetch.mockResponseOnce(JSON.stringify(productData));
-    render(<ProductsList />);
-
-    expect(screen.getByText("Products List")).toBeInTheDocument();
-    expect(screen.getByText("Loading products..")).toBeInTheDocument();
-    const addToCartButtons = await screen.findAllByText(/Add To Cart/i);
-    fireEvent.click(addToCartButtons[0]);
-    expect(screen.getByTestId("cart-button").textContent).toEqual("Cart (1)");
-    fireEvent.click(addToCartButtons[0]);
-    expect(screen.getByTestId("cart-button").textContent).toEqual("Cart (2)");
-  });
 });
